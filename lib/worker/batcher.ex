86bc29cf8a7c5f31cf93ecda8bfc7483721f32d0
Adam Neumann <adam.neumann@gusto.com>
2020-01-23 16:14:05 -0700

    Fix issue where a failed for non-ff batch would appear successful
    - issue where new status value not leaving scope branch expression
    - use new value to track the next value

--- lib/worker/batcher.ex
+++ lib/worker/batcher.ex
@@ -499,22 +499,26 @@ defmodule BorsNG.Worker.Batcher do
 
   defp maybe_complete_batch(batch) do
     statuses = Repo.all(Status.all_for_batch(batch.id))
-    status = Batcher.State.summary_database_statuses(statuses)
+    status_from_db = Batcher.State.summary_database_statuses(statuses)
     now = DateTime.to_unix(DateTime.utc_now(), :second)
-    if status != :running do
+    batch_status = if status_from_db != :running do
       # need to change status (could go from :ok to :error if non-ff push)
-      status = complete_batch(status, batch, statuses)
+      completed_status = complete_batch(status_from_db, batch, statuses)
 
       # status can change so send_status should come after complete_batch
       batch.project
       |> get_repo_conn()
-      |> send_status(batch, status)
+      |> send_status(batch, status_from_db)
       Project.ping!(batch.project_id)
+
+      completed_status
+    else
+      status_from_db
     end
     batch
-    |> Batch.changeset(%{state: status, last_polled: now})
+    |> Batch.changeset(%{state: batch_status, last_polled: now})
     |> Repo.update!()
-    if status != :running do
+    if batch_status != :running do
       poll_(batch.project_id)
     end
   end
